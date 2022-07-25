8d6e193af6d4c7314288706365a6ca25ddd5ba13
AvivBarel23 <avivbarel1998@gmail.com>
2022-07-25 21:33:37 +0300

    adding routes for single product

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,4 @@
-import products from "./data/products.js"
+import data from "./data.js"
 import express from "express"
 import dotenv from 'dotenv'
 import connectDatabase from "./config/MongoDb.js"
@@ -9,12 +9,19 @@ const app = express()
 
 //load products from server
 app.get("/api/products", (req, res) => {
-    res.json(products)
+    res.send(data.products)
 })
+
 //load single product from server
-app.get("/api/products/:id", (req, res) => {
-    const product = products.find((p) => p._id === req.params.id)
-    res.json(product)
+app.get("/api/products/slug/:slug", (req, res) => {
+    const product = data.products.find(p=> p.slug === req.params.slug);
+    if (product){
+        res.send(product)
+    }
+    else{
+        res.status(404).send({message:"Product doesn't exists"})
+    }
+
 })
 
 const PORT = process.env.PORT;
