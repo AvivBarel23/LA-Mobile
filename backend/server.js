a42694922bf761fc76849649341f5c2ff14a385d
AvivBarel23 <avivbarel1998@gmail.com>
2022-07-26 15:24:19 +0300

    API TO SPECIFIC PRODUCT

--- backend/server.js
+++ backend/server.js
@@ -24,6 +24,16 @@ app.get("/api/products/slug/:slug", (req, res) => {
 
 })
 
+app.get("/api/products/:id", (req, res) => {
+    const product = data.products.find(p=> p._id === req.params.id);
+    if (product){
+        res.send(product)
+    }
+    else{
+        res.status(404).send({message:"Product doesn't exists"})
+    }
+
+})
 const PORT = process.env.PORT;
 
 app.listen(PORT, console.log(`server running on port ${PORT}...`))
