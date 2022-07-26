145ebeae4ba6ca434cde914f82db94d29e79fb54
AvivBarel23 <avivbarel1998@gmail.com>
2022-07-26 15:23:05 +0300

    error and messages components

---
+++ frontend/src/components/MessageBox.js
@@ -0,0 +1,8 @@
+import {Alert} from "react-bootstrap";
+
+const MessageBox = (props) => {
+return(
+    <Alert variant={props.variant || 'info'}>{props.children}</Alert>
+)
+}
+export default MessageBox;
