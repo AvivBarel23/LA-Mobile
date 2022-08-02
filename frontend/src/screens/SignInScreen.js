fa1d37622770fdaa2f4b2b7b3f129bcbf0010c34
AvivBarel23 <avivbarel1998@gmail.com>
2022-08-02 14:15:14 +0300

    SIGN IN PAGE

--- frontend/src/screens/SignInScreen.js
+++ frontend/src/screens/SignInScreen.js
@@ -10,39 +10,36 @@ const SignInScreen = () => {
     const redirect = redirectInUrl ? redirectInUrl : '/';
 
     return (
-        <div>
-            <Container className="small-container">
-                <Helmet>
-                    <title>Sign In</title>
-                </Helmet>
-                <h1 className="my-3">Sign In</h1>
-                <Form>
-                    <Form.Group className="mb-3" controlId="email">
-                        <Form.label>
-                            Email
-                        </Form.label>
-                        <Form.Control type="email" required/>
+        <Container className="small-container">
+            <Helmet>
+                <title>Sign In</title>
+            </Helmet>
+            <h1 className="my-3">Sign In</h1>
+            <Form >
+                <Form.Group className="mb-3" controlId="email">
+                    <Form.Label>Email</Form.Label>
+                    <Form.Control type="email" required/>
+                </Form.Group>
 
-                    </Form.Group>
-                    <Form.Group className="mb-3" controlId="password">
-                        <Form.label>
-                            Password
-                        </Form.label>
-                        <Form.Control type="password" required/>
-                    </Form.Group>
+                <Form.Group className="mb-3" controlId="password">
+                    <Form.Label>Password</Form.Label>
+                    <Form.Control type="password" required
+                    />
+                </Form.Group>
 
-                    <div className="mb-3">
-                        <Button type="submit">Sign In</Button>
-                    </div>
+                <div className="mb-3">
+                    <Button type="submit">Sign In</Button>
+                </div>
 
-                    <div className="mb-3">
-                        New Costumer ? {'  '}
-                        <Link to={`/signup?redirect=${redirect}`}>Create your account</Link>
-                    </div>
-                </Form>
-            </Container>
-        </div>
-    )
+                <div className="mb-3">
+                    New customer?{' '}
+                    <Link to={`/signup?redirect=${redirect}`}>Create your account</Link>
+                </div>
+
+            </Form>
+        </Container>
+    );
 }
 
 export default SignInScreen;
+
