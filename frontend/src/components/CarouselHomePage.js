a0df19afdd54971175e5ad5b66419cbf4be7360b
AvivBarel23 <avivbarel1998@gmail.com>
2022-08-02 13:06:18 +0300

    adding carousel to the homepage

--- frontend/src/components/CarouselHomePage.js
+++ frontend/src/components/CarouselHomePage.js
@@ -1,21 +1,24 @@
 import Carousel from 'react-bootstrap/Carousel';
+import {Link} from "react-router-dom";
 
 function CarouselHomePage(props) {
-    const {products}=props;
+    const {products} = props;
     return (
-        <Carousel >
-            {products.map(p=>(
+        <Carousel>
+            {products.map(p => (
                 <Carousel.Item interval={1000}>
-                <img
-                    className="rounded mx-auto d-block"
-                    src={p.image}
-                    alt="slide"
-                />
-                <Carousel.Caption>
-                    <h3>{p.name}</h3>
-                    <p>{p.description}</p>
-                </Carousel.Caption>
-            </Carousel.Item>))}
+                    <Link to={`/products/${p.slug}`}>
+                        <img
+                            className="rounded mx-auto d-block"
+                            src={p.image}
+                            alt="slide"
+                        />
+                        <Carousel.Caption>
+                            <h3>{p.name}</h3>
+                            <p>{p.description}</p>
+                        </Carousel.Caption>
+                    </Link>
+                </Carousel.Item>))}
 
         </Carousel>
     );
