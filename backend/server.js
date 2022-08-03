b43c39526f274b7f749439eb6fb9af00bc93e2d0
AvivBarel23 <avivbarel1998@gmail.com>
2022-08-03 14:26:21 +0300

    generate token , decrypt passwords , user schema , product schema , seeding into db, seperating to routes folder

--- backend/server.js
+++ backend/server.js
@@ -3,15 +3,15 @@ import dotenv from "dotenv";
 import connectDatabase from "./config/MongoDb.js";
 import productRouter from "./routes/productRoutes.js";
 import userRouter from "./routes/userRoutes.js";
-import seedRouter from  "./routes/seedRoutes.js";
+import seedRouter from "./routes/seedRoutes.js";
 
 dotenv.config();
 connectDatabase();
 const app = express();
 
-app.use('/api/seed',seedRouter)
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use("/api/seed", seedRouter);
 app.use("/api/products", productRouter);
 app.use("/api/users", userRouter);
 
