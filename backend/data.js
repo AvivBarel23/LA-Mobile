715ad8a8aaa9f045994f500e02fcd05fed0b5161
AvivBarel23 <avivbarel1998@gmail.com>
2022-08-03 11:52:43 +0300

    new users

--- backend/data.js
+++ backend/data.js
@@ -1,10 +1,24 @@
 const data = {
+    users: [
+        {
+            name: 'Admin',
+            email: 'admin@gmail.com',
+            password: bcrypt.hashSync('Admin'),
+            isAdmin:true
+        },
+        {
+            name: 'Aviv',
+            email: 'avivbarel1998@gmail.com',
+            password: bcrypt.hashSync('AvivTheKing123'),
+            isAdmin:false
+        }
+    ],
     products: [
 
         {
             _id: "1",
             name: "iPhone 13 pro max 256gb",
-            slug:"iphone-13-pro-max",
+            slug: "iphone-13-pro-max",
             image: "/images/iphone-13-pro-max.jpg",
             description: "Iphone 13 pro max 256gb best iphone ever",
             price: 990,
@@ -15,7 +29,7 @@ const data = {
         {
             _id: "2",
             name: "iPhone 13 64gb",
-            slug:"iphone-13",
+            slug: "iphone-13",
             image: "/images/iphone-13.jpg",
             description: "Iphone 13with smaller screen",
             price: 700,
@@ -26,7 +40,7 @@ const data = {
         {
             _id: "3",
             name: "iPad pro 12.9 inch",
-            slug:"ipad-pro-12-9",
+            slug: "ipad-pro-12-9",
             image: "/images/ipad-pro-12-9.jpg",
             description: "huge screen for an ipad ",
             price: 1200,
@@ -37,7 +51,7 @@ const data = {
         {
             _id: "4",
             name: "macbook pro 14 inch",
-            slug:"macbook-pro-13",
+            slug: "macbook-pro-13",
             image: "/images/macbook-pro-14.jpg",
             description: "macbook pro with retina display",
             price: 2000,
@@ -48,7 +62,7 @@ const data = {
         {
             _id: "5",
             name: "airpods pro",
-            slug:"airpods-pro",
+            slug: "airpods-pro",
             image: "/images/airpods-pro.jpg",
             description: "airpods pro best earphones",
             price: 200,
