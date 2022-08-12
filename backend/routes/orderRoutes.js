4da52801c40438394818d116bbd9d042c256247e
LiorLL <lior3122@gmail.com>
2022-08-13 02:53:32 +0300

    Added missing files

--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -24,4 +24,34 @@ orderRouter.post(
   })
 );
 
+orderRouter.get(
+  '/:id',
+  isAuth,
+  expressAsyncHandler(async (req, res) => {
+    const order = await Order.findById(req.params.id);
+    if (order) {
+      res.send(order);
+    } else {
+      res.status(404).send({ message: 'Order not found' });
+    }
+  })
+);
+
+orderRouter.put(
+  '/:id/pay',
+  isAuth,
+  expressAsyncHandler(async (req, res) => {
+    const order = await Order.findById(req.params.id);
+    if (order) {
+      order.isPaid = true;
+      order.paidAt = Date.now();
+
+      const updatedOrder = await order.save();
+      res.send({ message: 'Order paid', order: updatedOrder });
+    } else {
+      res.status(404).send({ message: 'Order not found' });
+    }
+  })
+);
+
 export default orderRouter;
