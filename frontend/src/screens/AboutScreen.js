c940cce15512345c9272e3f0d0e35ff8c62124d3
LiorLL <lior3122@gmail.com>
2022-08-13 21:52:40 +0300

    Updated about screen

--- frontend/src/screens/AboutScreen.js
+++ frontend/src/screens/AboutScreen.js
@@ -5,19 +5,18 @@ export default function AboutScreen() {
   return (
     <Container className="small-container">
       <Helmet>
-        <title>About us </title>
+        <title>About us</title>
       </Helmet>
       <h1 className="my-3"> LA - Mobile </h1>
       <div>
-        The story behind LA - Mobile starts around 2019, when two entrepreneurs
-        Aviv Barel(Backend engineer) and Lior Levi Landman(Software Engineer and
-        Security researcher) met at Reichman university camups and beacme close
-        friends.LA comes from their initials and that is how the name was
-        chosen.Both of them are technology geeks and apple admires.They opend LA
-        - Mobile because they wanted to bring the best products, in the best
-        prices around israel. You can find in our store technology products -
-        from cameras to laptops and especially smartphones and mobiles(Apple
-        Iphones, google pixels, Samsung, etc).
+        The story behind LA-Mobile begins around 2019, when two entrepreneurs
+        Aviv(Backend engineer) and Lior(Software Engineer) met at Reichman
+        University and beacme friends. LA comes from their initials and that is
+        how the name was chosen. Both of them are tech-geeks and Apple fans.
+        They founded LA-Mobile to bring you the best products for the best
+        price. You can find many products in our store - from Cameras to Laptops
+        and Mobile phones(Apple Iphones,Google Pixel, Samsung Galaxy, and many
+        more).
       </div>
     </Container>
   );
